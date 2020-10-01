af4980bcdde7717be9e2669100e92af4b0e09190
Shashanka Prajapati <shashanka@fusemachines.com>
2020-10-01 11:44:39 +0545

    test: complex calculator

--- python_lifecycle/calculator/complex.py
+++ python_lifecycle/calculator/complex.py
@@ -1,5 +1,9 @@
+import warnings
+
 import fire
 from loguru import logger
+
+import python_lifecycle
 
 
 class Calculator:
@@ -18,7 +22,16 @@ class Calculator:
 
     @staticmethod
     def div(a, b):
-        return a / b
+        env = python_lifecycle.ENV
+        logger.info(f"Environment: {env}")
+        logger.info(f"Dividing {a} by {b}")
+        if env == "production":
+            try:
+                return a / b
+            except ZeroDivisionError as e:
+                warnings.warn(str(e), RuntimeWarning)
+        else:
+            return a / b
 
 
 def main():
