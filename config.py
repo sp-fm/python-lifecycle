2a2cec78db3a1d52b53c691b941c59544d73db0a
Shashanka Prajapati <shashanka@fusemachines.com>
2020-10-01 11:27:18 +0545

    feat: Broken calculator with an offset from dynaconf

---
+++ config.py
@@ -0,0 +1,9 @@
+from dynaconf import Dynaconf
+
+settings = Dynaconf(
+    envvar_prefix="DYNACONF",
+    settings_files=["configs/settings.yaml", "configs/.secrets.yaml"],
+)
+
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `settings_files` = Load this files in the order.
