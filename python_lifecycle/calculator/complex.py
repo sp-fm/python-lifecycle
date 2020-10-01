458c0624aedb409998e6f6209f84204b14634b91
Shashanka Prajapati <shashanka@fusemachines.com>
2020-10-01 11:07:54 +0545

    feat: Command Line calculator with fire

---
+++ python_lifecycle/calculator/complex.py
@@ -0,0 +1,27 @@
+import fire
+
+
+class Calculator:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def sub(a, b):
+        return a - b
+
+    @staticmethod
+    def mul(a, b):
+        return a * b
+
+    @staticmethod
+    def div(a, b):
+        return a / b
+
+
+def main():
+    fire.Fire(Calculator)
+
+
+if __name__ == "__main__":
+    main()
--- python_lifecycle/calculator/simple.py
+++ python_lifecycle/calculator/simple.py
@@ -1,5 +1,15 @@
 from typing import Union
 
+import fire
+
 
 def add(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
-   return a + b
+    return a + b
+
+
+def main():
+    fire.Fire(add)
+
+
+if __name__ == "__main__":
+    main()
